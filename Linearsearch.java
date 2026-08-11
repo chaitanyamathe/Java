b4be22c19ef09840c0728dda045f3af5a4956ee5
chaitanyamathe <mathechaitanya@gmail.com>
2026-08-11 09:55:07 +0530

    Arrays

--- Linearsearch.java
+++ Linearsearch.java
@@ -1,31 +1,20 @@
+package Arrays;
 import java.util.Scanner;
-class Linearsearchconflict2{
-public static void main(String args[]){
-int[] a=new int[5];
-int flag=0;
-Scanner scan=new Scanner(System.in);
-for(int i=0;i<a.length;i++)
-{
-  System.out.println("Enter the "+(i+1)+" Element =");
-  a[i]=scan.nextInt();
-}
-System.out.println("Enter the search element = ");
-int search=scan.nextInt();
-for(int i=0;i<a.length;i++)
-{
-    if(search==a[i])
-     {
-       flag=1;
-       break;
-     }
-}
-if(flag==1)
-{
-  System.out.println("Element "+search+" found ");
-}
-else
-{
- System.out.println("Element "+search+" not found ");
-}
-}
+import java.util.Arrays;
+
+public class LinearSearch {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of the array...");
+		int n=sc.nextInt();
+		
+		int[] arr=new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+			
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[arr.length-1]);
+	}
+
 }
